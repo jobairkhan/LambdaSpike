e07bc19e9ea6a726bcb8365899d81926da83a5b1
Jobair Khan <jobair.khan@groupimd.com>
2018-08-07 16:52:40 +0100

    Lambda  processing batch messages

---
+++ LambdaWithSQS/LambdaWithSQS.Tests/LambdaFunctionTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Lambda.SQSEvents;
+
+using Amazon.Lambda.TestUtilities;
+using Xunit;
+
+namespace LambdaWithSQS.Tests
+{
+    public class LambdaFunctionTest
+    {
+        [Fact]
+        public async Task TestSQSEventLambdaFunction()
+        {
+            var sqsMessage = new SQSEvent.SQSMessage
+            {
+                Body = "foobar"
+            };
+            sqsMessage.Attributes = new Dictionary<string, string>
+            {
+                {"attr", "Test"}
+            };
+            var sqsEvent = new SQSEvent
+            {
+                Records = new List<SQSEvent.SQSMessage>
+                {
+                    sqsMessage
+                }
+            };
+
+            var logger = new TestLambdaLogger();
+            var context = new TestLambdaContext
+            {
+                Logger = logger
+            };
+
+            var function = new LambdaFunction();
+            await function.Handler(sqsEvent, context);
+
+            Assert.Contains("attr: Test", logger.Buffer.ToString());
+        }
+    }
+}
--- LambdaWithSQS/LambdaWithSQS/LambdaFunction.cs
+++ LambdaWithSQS/LambdaWithSQS/LambdaFunction.cs
@@ -8,18 +8,18 @@ using Amazon.Lambda.SQSEvents;
 
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-//[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace LambdaWithSQS
 {
-    public class Function
+    public class LambdaFunction
     {
         /// <summary>
         /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
         /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
         /// region the Lambda function is executed in.
         /// </summary>
-        public Function()
+        public LambdaFunction()
         {
 
         }
@@ -32,22 +32,26 @@ namespace LambdaWithSQS
         /// <param name="evnt"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
+        public async Task Handler(SQSEvent evnt, ILambdaContext context)
         {
-            foreach(var message in evnt.Records)
+            var recordCount = evnt.Records.Count;
+            var processId = Guid.NewGuid();
+            foreach (var message in evnt.Records)
             {
-                await ProcessMessageAsync(message, context);
+                context.Logger.LogLine($"Processing message {recordCount} - Id {processId}");
+                await ProcessLambdaMessageAsync(message, context);
             }
         }
 
-        private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+        private async Task ProcessLambdaMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
         {
             if (message.Body.Contains("Error:")) throw new Exception();
 
             context.Logger.LogLine($"Processed message {message.Body}");
-            
+            context.Logger.LogLine("Attributes");
+            context.Logger.LogLine("----------");
+            context.Logger.LogLine(string.Join(",", message.Attributes.Select(s => $"{s.Key}: {s.Value}")));
 
-            // TODO: Do interesting work based on the new message
             await Task.CompletedTask;
         }
     }
